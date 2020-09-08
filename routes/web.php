60ada3eac8124e70acd6b417f3b8370228717f91
sherifNabil <sherifNabil@gitlab.com>
2020-09-08 20:25:19 +0200

    routes modifying

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('threads', 'ThreadsController@index');
-Route::get('threads/{thread}', 'ThreadsController@show');
-Route::post('threads/{thread}/replies', 'RepliesController@store');
+Route::get('threads/{channel}/{thread}', 'ThreadsController@show');
+Route::get('threads/create', 'ThreadsController@create');
+Route::post('threads/', 'ThreadsController@store');
+Route::post('threads/{channel}/{thread}/replies', 'RepliesController@store');
