9877cfe8a889f63c3b263c805b8a7301afb906dd
sherifNabil <sherifNabil@gitlab.com>
2020-09-08 15:31:17 +0200

    customizing TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseMigrations;
+
+    protected function signIn($user = null)
+    {
+        $user = $user ?: create(User::class);
+        
+        $this->actingAs($user);
+        return $this;
+    }
 }
