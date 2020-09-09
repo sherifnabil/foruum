aaa21b5625da5aa93d9051dc00527297256f779c
sherifNabil <sherifNabil@gitlab.com>
2020-09-09 23:14:40 +0200

    switch from view share to view composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \view()->share('channels', Channel::all());
+        \view()->composer('*', function ($view) {
+            $view->with('channels', Channel::all());
+        });
     }
 }
