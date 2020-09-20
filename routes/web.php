11936eded738dffdf126fd174714f2cfc728c030
sherifNabil <sherifNabil@gitlab.com>
2020-09-20 06:16:22 +0200

    adding favoriting for reply

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::post('threads', 'ThreadsController@store');
 Route::get('threads/{channel}/{thread}', 'ThreadsController@show');
 
 Route::post('threads/{channel}/{thread}/replies', 'RepliesController@store');
+
+Route::post('replies/{reply}/favorites', 'FavoritesController@store');
