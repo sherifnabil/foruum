3d94ab1125659e4414d70517f1f8b75ef17a504b
sherifNabil <sherifNabil@gitlab.com>
2020-09-26 06:13:13 +0200

    thread flash message

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -46,7 +46,8 @@ class ThreadsController extends Controller
             'channel_id'    => request('channel_id'),
         ]);
 
-        return redirect($thread->path());
+        return redirect($thread->path())
+        ->with('flash', 'Your thread has been published');
     }
    
     public function show($channel, Thread $thread)
