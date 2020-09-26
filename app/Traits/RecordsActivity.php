2e8f8111d3013b37ea0639663b081499bc2ead41
sherifNabil <sherifNabil@gitlab.com>
2020-09-26 06:13:56 +0200

    delete activity

--- app/Traits/RecordsActivity.php
+++ app/Traits/RecordsActivity.php
@@ -24,6 +24,10 @@ trait RecordsActivity
                 
             });
         }
+
+        static::deleting(function ($model) {
+            $model->activity()->delete();
+        });
     }
 
     protected static function getActivitiesToRecord()
